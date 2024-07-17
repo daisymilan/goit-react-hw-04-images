7089dfd8bf19d5a475385205da462cf714c9d8d0
daisymilan <daisymilan3@gmail.com>
2024-07-17 22:29:00 +0800

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,43 +2,34 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
 
-class SearchBar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+const SearchBar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+};
 
-  state = { query: '' };
+const handleSubmit = event => {
+  event.preventDefault();
+  onSubmit(query);
+  setQuery('');
+};
 
-  handleChange = event => {
-    this.setState({ query: event.target.value });
-  };
+return (
+    <header className={styles.searchbar}>
+      <form className={styles.form} onSubmit={this.handleSubmit}>
+        <button type="submit" className={styles.button}>
+          <span className={styles.buttonLabel}>Search</span>
+        </button>
+        <input
+          className={styles.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={this.state.query}
+          onChange={this.handleChange}
+        />
+      </form>
+    </header>
+);
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
-  };
-
-  render() {
-    return (
-      <header className={styles.searchbar}>
-        <form className={styles.form} onSubmit={this.handleSubmit}>
-          <button type="submit" className={styles.button}>
-            <span className={styles.buttonLabel}>Search</span>
-          </button>
-          <input
-            className={styles.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
 
 export default SearchBar;
