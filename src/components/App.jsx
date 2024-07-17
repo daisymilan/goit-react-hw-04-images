11ec83e0f31fb41f0778cccfb219cfe331c31c0b
daisymilan <daisymilan3@gmail.com>
2024-07-17 19:17:12 +0800

    starter pack (from hw 3)

---
+++ src/components/App.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import SearchBar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import { getAPI } from 'pixabay-api';
+import styles from './App.module.css';
+import toast, { Toaster } from 'react-hot-toast';
+
+class App extends Component {
+  state = {
+    images: [],
+    currentPage: 1,
+    searchQuery: '',
+    isLoading: false,
+    isError: false,
+    isEnd: false,
+  };
+
+  async componentDidUpdate(_prevProps, prevState) {
+    const { searchQuery, currentPage } = this.state;
+
+    if (
+      prevState.searchQuery !== searchQuery ||
+      prevState.currentPage !== currentPage
+    ) {
+      await this.fetchImages();
+    }
+  }
+
+  fetchImages = async () => {
+    const { searchQuery, currentPage } = this.state;
+
+    this.setState({ isLoading: true });
+
+    try {
+      const response = await getAPI(searchQuery, currentPage);
+
+      console.log(response);
+
+      const { totalHits, hits } = response;
+
+      if (hits.length === 0) {
+        toast.error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        this.setState({ isLoading: false });
+        return;
+      }
+
+      if (currentPage === 1) {
+        toast.success(`Hooray! We found ${totalHits} images!`);
+      }
+
+      if (currentPage * 12 >= totalHits) {
+        this.setState({ isEnd: true });
+        toast("We're sorry, but you've reached the end of search results.", {
+          icon: '👏',
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        });
+      }
+
+      this.setState(prevState => ({
+        images: currentPage === 1 ? hits : [...prevState.images, ...hits],
+        isEnd: prevState.images.length + hits.length >= totalHits,
+      }));
+    } catch (error) {
+      this.setState({ isError: true });
+      toast.error('Oops, something went wrong! Reload this page!');
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  handleSearchSubmit = query => {
+    const normalizedQuery = query.trim().toLowerCase();
+    const normalizedCurrentQuery = this.state.searchQuery.toLowerCase();
+
+    if (normalizedQuery === '') {
+      alert(`Empty string is not a valid search query. Please type again.`);
+      return;
+    }
+
+    if (normalizedQuery === normalizedCurrentQuery) {
+      alert(
+        `Search query is the same as the previous one. Please provide a new search query.`
+      );
+      return;
+    }
+
+    if (normalizedQuery !== normalizedCurrentQuery) {
+      this.setState({
+        searchQuery: normalizedQuery,
+        currentPage: 1,
+        images: [],
+        isEnd: false,
+      });
+    }
+  };
+
+  handleLoadMore = () => {
+    if (!this.state.isEnd) {
+      this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
+    } else {
+      alert("You've reached the end of the search results.");
+    }
+  };
+
+  render() {
+    const { images, isLoading, isError, isEnd } = this.state;
+    return (
+      <div className={styles.App}>
+        <SearchBar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} />
+        {isLoading && <Loader />}
+        {!isLoading && !isError && images.length > 0 && !isEnd && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+        {isError && <p>Something went wrong. Please try again later.</p>}
+        <Toaster position="top-right" reverseOrder={false} />
+      </div>
+    );
+  }
+}
+
+export default App;
